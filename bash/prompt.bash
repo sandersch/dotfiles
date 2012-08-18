7021a77401d83c5bd2a8393961e1d695632e50ef
Charlie Sanders <sanderscharlie@gmail.com>
2012-08-17 22:07:41 -0500

    Use git topic folder for git-prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-source ~/dotfiles/git-prompt/git-prompt.sh
+source ~/dotfiles/git/prompt/git-prompt.sh
 source ~/dotfiles/git/completion.bash
 
 # Enable vi style line editing mode (ksh broke my brain)
