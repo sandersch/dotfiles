9232ff4db257ad39f6abbe5333d8ebbb5498cdd6
Charlie Sanders <sanderscharlie@gmail.com>
2012-08-20 01:09:40 -0500

    Adds some git aliases

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,2 +1,5 @@
 alias gst='git status'
 alias update_submodules='git pull --recurse-submodules && git submodule update'
+alias gp='git push origin HEAD'
+alias gb='git co -b'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
