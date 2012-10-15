6c6f33dba5f9eda8576eb21be95ff79556b390e6
Charlie Sanders <sanderscharlie@gmail.com>
2012-10-14 21:35:00 -0500

    DRY up config for color CLI command bash aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,26 +1,27 @@
 # enable color support of ls if it seems available
 export GREP_COLOR=33
+USE_COLOR_CMDS=""
 if [ -x /usr/bin/dircolors ]; then
+    USE_COLOR_CMDS="true"
+    LS_CMD='ls --classify --color=auto'
+
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
-
-    # Highlight search term in grep
-    alias grep='grep --color=auto --line-number'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-
-    LS_CMD='ls --classify --color=auto'
 elif $(gls &> /dev/null); then
-    # Highlight search term in grep
-    alias grep='grep --color=auto --line-number'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-
+    USE_COLOR_CMDS="true"
     LS_CMD='gls --classify --color=auto'
 else
     LS_CMD='ls'
 fi
+
+if [ ! -z "$USE_COLOR_CMDS" ]; then
+    # Highlight search term in grep
+    alias grep='grep --color=auto --line-number'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+unset USE_COLOR_CMDS
 
 alias ls="$LS_CMD"
 alias ll="$LS_CMD -al"
