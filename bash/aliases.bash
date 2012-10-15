06efab77e0e56062125a2c1399d1154e82769be6
Charlie Sanders <sanderscharlie@gmail.com>
2012-10-14 21:39:29 -0500

    Remove aliases from main file that were moved to multimedia file

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -40,23 +40,9 @@ alias rm='rm -i'
 alias cp='cp -v'
 alias mv='mv -v'
 
-# Run mplayer full screen on screen 0 and turn off the screensaver
-alias mplayerfs='mplayer -fs -zoom -stop-xscreensaver'
-alias mplayerfs0='mplayer -xineramascreen 0 -fs -zoom -stop-xscreensaver'
-alias mplayerfs1='mplayer -xineramascreen 1 -fs -zoom -stop-xscreensaver'
-alias mplayerfs2='mplayer -xineramascreen 2 -fs -zoom -stop-xscreensaver'
-alias mplayerfs3='mplayer -xineramascreen 3 -fs -zoom -stop-xscreensaver'
-alias mythfs='mythfrontend -nw -geometry 1920x1080+0+0'
-
-alias lmp3="ls *.mp3 > list && chmod a+x list && vim list"
-
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
-burndir() { 
-    growisofs -dvd-compat -Z /dev/dvdrw3 -R -J -V "$1" "$1" && mplayer /usr/share/sounds/KDE-Im-Phone-Ring.ogg
-}
 
 for FILE in ~/dotfiles/*/aliases.sh
 do
