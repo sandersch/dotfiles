6f57c244d0b5a61228c7e501f10b67b49aed6820
Charlie Sanders <sanderscharlie@gmail.com>
2012-10-14 19:27:45 -0500

    Adds aliases for common ruby/rails commands

---
+++ ruby/aliases.sh
@@ -0,0 +1,8 @@
+# Rails Commands
+alias r='bundle exec rails'
+alias rg='bundle exec rails g'
+alias rs='bundle exec rails s'
+alias rc='bundle exec rails c'
+alias rn='bundle exec rails new'
+alias dbm='bundle exec rake db:migrate'
+alias rlog='tail -f log/development.log'
