e47c20a867c4cb38f6b187377590dc8c7c3994b0
Charlie Sanders <sanderscharlie@gmail.com>
2012-10-14 22:18:36 -0500

    Update mythfs alias to use screen 2

--- multimedia/aliases.sh
+++ multimedia/aliases.sh
@@ -4,7 +4,7 @@ alias mplayerfs0='mplayer -xineramascreen 0 -fs -zoom -stop-xscreensaver'
 alias mplayerfs1='mplayer -xineramascreen 1 -fs -zoom -stop-xscreensaver'
 alias mplayerfs2='mplayer -xineramascreen 2 -fs -zoom -stop-xscreensaver'
 alias mplayerfs3='mplayer -xineramascreen 3 -fs -zoom -stop-xscreensaver'
-alias mythfs='mythfrontend -nw -geometry 1920x1080+0+0'
+alias mythfs='mythfrontend -nw -geometry 1920x1080+3840+0'
 
 alias lmp3="ls *.mp3 > list && chmod a+x list && vim list"
 
