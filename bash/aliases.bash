edf4421be31d0b27c7f91db8a565303c878f7275
Charlie Sanders <sanderscharlie@gmail.com>
2013-02-21 20:38:59 -0600

    Adds system update commands for Debian and Redhat based systems

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -44,6 +44,13 @@ alias mv='mv -v'
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+# Fully update operating system
+if [ -f /etc/debian_version ]; then
+    alias update_system="sudo bash -c 'apt-get -y update && apt-get -y upgrade && apt-get -y dist-upgrade && apt-get -y autoremove'"
+elif [ -f /etc/redhat-release ]; then
+    alias update_system="sudo bash -c 'yum -y update'"
+fi
+
 # Simulate OSX's pbcopy and pbpaste on other platforms
 if [ ! $(uname -s) = "Darwin" ]; then
     alias pbcopy='xsel --clipboard --input'
