6e7ed5496504405b5a1175d0609029994564cd32
Charlie Sanders <sanderscharlie@gmail.com>
2025-08-20 17:39:11 -0500

    remove heroku push git alias

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,6 +1,5 @@
 alias gst='git status'
 alias update_submodules='git pull --recurse-submodules && git submodule update'
 alias gp='git push origin HEAD'
-alias gph='git push heroku HEAD'
 alias gb='git co -b'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
