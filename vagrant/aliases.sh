bcfff75002bc00de8d1a4f68c4535a7209116052
Charlie Sanders <sanderscharlie@gmail.com>
2025-08-20 17:38:34 -0500

    remove vagrant shell aliases

--- vagrant/aliases.sh
+++
@@ -1,6 +0,0 @@
-alias vs="vagrant status"
-alias vu="vagrant up"
-alias vp="vagrant provision"
-alias vr="vagrant reload"
-alias vssh="vagrant ssh"
-alias vdu="vagrant destroy --force && vagrant up"
