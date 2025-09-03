255db3f5928ddfa3f783d5d5b4cfe29dea2e7c80
Charlie Sanders <sanderscharlie@gmail.com>
2025-09-03 10:55:18 -0500

    readonly alias for monorail replica db

--- bash/local/st-ccs3.bash
+++ bash/local/st-ccs3.bash
@@ -8,3 +8,5 @@ export DOCKERIZED_PG_DUMP=true
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+alias readonly="PROD_REPLICA_PASSWORD='op://p3hh25rpgtgb6codrqakfh2o3y/vy6j52fj24tf6lox2fvsm4rnhu/password' op run --no-masking -- bin/rails_console_production_replica"
