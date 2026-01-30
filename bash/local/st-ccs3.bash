08c7a14c00b56a5330716842b84b646dbb6188fe
Charlie Sanders <sanderscharlie@gmail.com>
2026-01-29 18:07:45 -0600

    bridge stuff

--- bash/local/st-ccs3.bash
+++ bash/local/st-ccs3.bash
@@ -1,12 +1,12 @@
-alias beets='srb && rubocop && bin/parallel_rspec'
-alias testdb='bin/rake parallel:drop parallel:setup'
-
-export PATH="$PATH:~/.cargo/bin/"
-
-export DOCKERIZED_PG_DUMP=true
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+export PATH="$PATH:~/.cargo/bin/"
+export PII_SCANNER_BYPASS_EPOCH=9999999999999
+
+export DOCKERIZED_PG_DUMP=true
+alias beets='srb && rubocop && bin/parallel_rspec'
+alias testdb='bin/rake parallel:drop parallel:setup'
 
 alias readonly="PROD_REPLICA_PASSWORD='op://p3hh25rpgtgb6codrqakfh2o3y/vy6j52fj24tf6lox2fvsm4rnhu/password' op run --no-masking -- bin/rails_console_production_replica"
--- git/aliases.sh
+++ git/aliases.sh
@@ -3,3 +3,5 @@ alias update_submodules='git pull --recurse-submodules && git submodule update'
 alias gp='git push origin HEAD'
 alias gb='git co -b'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gwab="git worktree add -b"
+alias gwro="git worktree remove $OLDPWD"
