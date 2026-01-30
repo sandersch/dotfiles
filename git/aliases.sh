2e92691253e9010d49d237fb5fdb61760a867d8f
Charlie Sanders <sanderscharlie@gmail.com>
2026-01-29 18:12:59 -0600

    gpf

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,6 +1,7 @@
 alias gst='git status'
 alias update_submodules='git pull --recurse-submodules && git submodule update'
 alias gp='git push origin HEAD'
+alias gpf='git push origin HEAD --force-with-lease'
 alias gb='git co -b'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gwab="git worktree add -b"
