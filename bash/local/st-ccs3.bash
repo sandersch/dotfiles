baaa2acf6ed61c49cbcffae464f6ab486f7194b5
Charlie Sanders <sanderscharlie@gmail.com>
2026-02-20 18:06:03 -0600

    st-ccs3

--- bash/local/st-ccs3.bash
+++ bash/local/st-ccs3.bash
@@ -2,7 +2,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-export PATH="$PATH:~/.cargo/bin/"
+export PATH="$PATH:~/.cargo/bin/:~/stripe/aop/target/release/aop"
 export PII_SCANNER_BYPASS_EPOCH=9999999999999
 
 export DOCKERIZED_PG_DUMP=true
@@ -10,3 +10,6 @@ alias beets='srb && rubocop && bin/parallel_rspec'
 alias testdb='bin/rake parallel:drop parallel:setup'
 
 alias readonly="PROD_REPLICA_PASSWORD='op://p3hh25rpgtgb6codrqakfh2o3y/vy6j52fj24tf6lox2fvsm4rnhu/password' op run --no-masking -- bin/rails_console_production_replica"
+
+export CLAUDE_CODE_DANGEROUSLY_ALLOWED_MCP_SERVERS="sentry"
+unset GH_HOST
